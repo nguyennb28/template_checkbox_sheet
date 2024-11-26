dfdc4fb3f070dde87a704647891ff4495f77a81f
nguyennb.work <nguyennb.work@gmail.com>
2024-11-26 08:23:48 +0700

    implement month and year selection with dynamic date generation; add styling for date table

--- public/index.js
+++ public/index.js
@@ -1,2 +1,65 @@
-console.log('Hello World');
-console.log('Hello World 3');
+const selectMonth = document.getElementById("list-month");
+const inputYear = document.getElementById("input-year");
+const tableDateTimes = document.getElementById("table-date-month-year");
+// list-date-month-year
+const currentYear = new Date().getFullYear();
+inputYear.value = currentYear;
+// Get list month
+const listMonth = function () {
+  for (let index = 12; index > 0; index--) {
+    selectMonth.innerHTML += `<option value="${index}">${index}</option>`;
+  }
+};
+// run listMonth()
+listMonth();
+
+let days = getDateByMonthAndYear(inputYear.value, selectMonth.value);
+listDateMonthYear(days);
+// Get current year
+selectMonth.addEventListener("change", function (e) {
+  if (inputYear.value) {
+    days = getDateByMonthAndYear(inputYear.value, e.target.value);
+    listDateMonthYear(days);
+  }
+});
+inputYear.addEventListener("keyup", function (e) {
+  if (e.target.value.length == 4) {
+    days = getDateByMonthAndYear(e.target.value, selectMonth.value);
+    listDateMonthYear(days);
+    // change title year
+    const titleYear = document.querySelector(".title-year");
+    titleYear.textContent = e.target.value;
+  }
+});
+
+// get date of month and year
+function getDateByMonthAndYear(year, month) {
+  if (year && month) {
+    const date = new Date(year, month, 0);
+    const days = date.getDate();
+    return days;
+  }
+  return 0;
+}
+
+function listDateMonthYear(days) {
+  const listDate = document.querySelector(".list-date-month-year");
+  const listCheckbox = document.querySelector(".list-checkbox");
+  listDate.innerHTML = "";
+  listCheckbox.innerHTML = "";
+  for (let index = days; index > 0; index--) {
+    listDate.innerHTML += `<td>
+    ${new Date(
+      inputYear.value,
+      selectMonth.value - 1,
+      index
+    ).toLocaleDateString("vi-VN", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    })}
+    </td>`;
+    listCheckbox.innerHTML += `<td><input type="checkbox" name="date" value="${index}-${selectMonth.value}-${inputYear.value}" /></td>`;
+  }
+}
